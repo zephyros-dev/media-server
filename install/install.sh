e03facd86b3628fa6fe88b5e10c92e48c7b01371
phong.ngthanh <phong.ngthanh@outlook.com>
2021-05-25 09:04:48 +0700

    1. Change configuration role name to maintenance

--- install/install.sh
+++ install/install.sh
@@ -2,8 +2,8 @@
 # Ansible
 # sudo dnf install -y ansible
 pip install ansible
-# For ansible enter password, but probably already have
+# For ansible enter password
 sudo dnf install -y sshpass
 # Extra package for ansible
-ansible-galaxy install -r requirements.yaml
+# ansible-galaxy install -r requirements.yaml
 ansible-galaxy collection install -r requirements.yaml
