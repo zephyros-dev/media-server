53941d651c39b562f915b15632cde31864525927
phong.ngthanh <phong.ngthanh@outlook.com>
2021-10-08 23:19:40 +0700

    Change pod name

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Ansible
 # sudo dnf install -y ansible
-pip install ansible
+# pip install ansible
 # For ansible enter password
 sudo dnf install -y sshpass
 # Extra package for ansible
-ansible-galaxy install -r requirements.yaml
+ansible-galaxy install -r requirements.yaml --force
