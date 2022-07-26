cc667ec97f9984b8e271b2c384c18b00164c8035
phong.ngthanh <phong.ngthanh@outlook.com>
2022-07-26 04:07:59 +0000

    Add ansible

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo chown -R $USER:$USER ~/.local
+sudo chown -R $USER:$USER ~/.local ~/.config
--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Ansible
-# sudo dnf install -y ansible
-# pip install ansible
-# For ansible enter password
-sudo dnf install -y sshpass
 # Extra package for ansible
 ansible-galaxy install -r requirements.yaml --force-with-deps
