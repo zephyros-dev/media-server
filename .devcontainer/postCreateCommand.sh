5b3025f974c9fbda24391c856d0baa94b646ef6b
phong.ngthanh <phong.ngthanh@outlook.com>
2022-10-02 10:20:44 +0700

    feat: add docker in docker for molecule

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo chown $USER:$USER ~/.local ~/.config
+sudo chown ${USER}:${USER} ~/.local ~/.config
+sudo chown -R ${USER}:${USER} ~/.local/share/fish
 ansible-galaxy install -r requirements.yaml
