d7c15bb5d334649e8666c2d490c7e3feb6803287
phong.ngthanh <phong.ngthanh@outlook.com>
2022-10-30 15:06:31 +0000

    feat: add pre-commit check

---
+++ .devcontainer/initializeCommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch ~/.gitconfig
+mkdir -p \
+    ~/.config/sops/age \
+    ~/.local/share/fish \
+
+touch ~/.config/sops/age/keys.txt
--- .devcontainer/postCreateCommand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo chown ${USER}:${USER} ~/.local ~/.config
-sudo chown -R ${USER}:${USER} ~/.local/share/fish
-ansible-galaxy install -r requirements.yaml
