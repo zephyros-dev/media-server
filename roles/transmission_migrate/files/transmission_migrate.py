dcb83a14ef3c75de74bec97a4981db4f323a567a
phong.ngthanh <phong.ngthanh@outlook.com>
2022-11-16 09:02:44 +0000

    fix: transmission migrate --dy-run

--- roles/transmission_migrate/files/transmission_migrate.py
+++ roles/transmission_migrate/files/transmission_migrate.py
@@ -18,7 +18,7 @@ parser.add_argument(
     "--port", type=int, default=443, help="Port of the transmission server"
 )
 parser.add_argument(
-    "--dry-run", type=bool, default=True, help="Print the result of the run only"
+    "--dry-run", action="store_true", help="Print the result of the run only"
 )
 parser.add_argument(
     "--protocol",
@@ -41,26 +41,25 @@ c = Client(
 )
 
 if args.new_path.__contains__(args.old_path):
-    args.dry_run = True
     print(
         f"Be careful for path replacement since new path: {args.new_path} contain old path: {args.old_path}. Forcing dry-run"
     )
+    args.dry_run = True
 
 torrent_list = c.get_torrents()
 torrent_dict = {}
 
 for torrent in torrent_list:
-    new_dir = torrent.download_dir.replace(args.old_path, args.new_path, 1)
-    torrent_dict[torrent.id] = {
-        "name": torrent.name,
-        "old_path": torrent.download_dir,
-        "new_path": new_dir,
-    }
-    if not args.dry_run:
-        c.locate_torrent_data(ids=torrent.id, location=new_dir)
-    print(
-        f"Torrent ID: {torrent.id}, Torrent Name: {torrent.name}, Old path: {torrent.download_dir}, New path: {new_dir}"
-    )
+    if args.old_path in torrent.download_dir:
+        new_dir = torrent.download_dir.replace(args.old_path, args.new_path, 1)
+        torrent_dict[torrent.id] = {
+            "name": torrent.name,
+            "old_path": torrent.download_dir,
+            "new_path": new_dir,
+        }
+        if not args.dry_run:
+            c.locate_torrent_data(ids=torrent.id, location=new_dir)
+
 now = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
 out_file = open(f"{args.output_json_file}.{now}.json", "w")
 jsonStr = json.dump(torrent_dict, out_file, sort_keys=True, indent=2)
