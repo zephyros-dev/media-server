97d8b56b5d41c5efa057a381914aa4d26341e3ea
zhex <dev@zhex.cc>
2024-09-18 18:01:16 +0700

    ci: separate run and retry command

---
+++ ci/retry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wg-quick down wg0 && wg-quick up wg0 &> /dev/null
+# https://docs.dagger.io/manuals/user/troubleshooting/#dagger-is-unable-to-resolve-host-names-after-network-configuration-changes
+DAGGER_ENGINE_DOCKER_CONTAINER="$(docker container list --all --filter 'name=^dagger-engine-*' --format '{{.Names}}')"
+docker restart "$DAGGER_ENGINE_DOCKER_CONTAINER"
