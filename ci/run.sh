a9212399b5e2dc029b63bfc50bf8688602271110
zhex <dev@zhex.cc>
2024-09-18 17:50:52 +0700

    ci: retry run on retry

--- ci/run.sh
+++ ci/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 wg-quick down wg0 && wg-quick up wg0 &> /dev/null
+eval `ssh-agent`
+sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
 # https://docs.dagger.io/manuals/user/troubleshooting/#dagger-is-unable-to-resolve-host-names-after-network-configuration-changes
 DAGGER_ENGINE_DOCKER_CONTAINER="$(docker container list --all --filter 'name=^dagger-engine-*' --format '{{.Names}}')"
 docker restart "$DAGGER_ENGINE_DOCKER_CONTAINER"
