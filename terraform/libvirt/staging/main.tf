b970311db34151c79016a197ad23a6ad45536af4
zhex <dev@zhex.cc>
2024-09-24 22:07:47 +0700

    chore: add static wwn for libvirt disks

--- terraform/libvirt/staging/main.tf
+++ terraform/libvirt/staging/main.tf
@@ -1,22 +1,35 @@
 
 #region staging-iot
-resource "libvirt_volume" "data" {
-  count  = 2
-  name   = "staging-data-${count.index}"
-  size   = 10000000000
-  format = "raw"
+resource "libvirt_volume" "this" {
+  for_each = local.disk_set
+  name     = each.key
+  size     = 6 * 1024 * 1024 * 1024
+  format   = "raw"
 }
 
-resource "libvirt_volume" "parity" {
-  count  = 1
-  name   = "staging-parity-${count.index}"
-  size   = 10000000000
-  format = "raw"
+locals {
+  # Use specific
+  disk = {
+    data = [
+      "0x05abcd1fb3191a0d",
+      "0x05abcdd9f6a92032"
+    ]
+    parity = [
+      "0x05abcd6b765b3899"
+    ]
+  }
+  disk_set = merge(flatten([
+    for type, wwn_list in local.disk : [
+      for index, wwn in wwn_list : {
+        "staging-${type}-${index}" : wwn
+      }
+    ]
+  ])...)
 }
 
 resource "libvirt_volume" "root" {
   name   = "staging-root"
-  size   = 50000000000
+  size   = 36 * 1024 * 1024 * 1024
   format = "raw"
 }
 
@@ -44,19 +57,15 @@ resource "libvirt_domain" "this" {
 
   disk {
     volume_id = libvirt_volume.root.id
+    scsi      = true
   }
 
   dynamic "disk" {
-    for_each = libvirt_volume.data
+    for_each = local.disk_set
     content {
-      volume_id = disk.value.id
-    }
-  }
-
-  dynamic "disk" {
-    for_each = libvirt_volume.parity
-    content {
-      volume_id = disk.value.id
+      volume_id = libvirt_volume.this[disk.key].id
+      scsi      = true
+      wwn       = disk.value
     }
   }
 
