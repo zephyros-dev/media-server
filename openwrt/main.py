7e262f9d20b7748f72b91366c0f269873c77010e
zhex <dev@zhex.cc>
2024-12-03 21:07:26 +0700

    feat: update zapret to not use submodule

--- openwrt/main.py
+++ openwrt/main.py
@@ -7,33 +7,36 @@ import subprocess
 import sys
 from pathlib import Path
 
+import yaml
 from dotenv import set_key
 
 os.chdir(Path(sys.argv[0]).parent)
 
-openwrt_config = json.loads(
-    subprocess.run(["sops", "-d", "config.sops.json"], capture_output=True).stdout
+openwrt_config = yaml.safe_load(
+    subprocess.run(
+        ["sops", "-d", "config.sops.yaml"], capture_output=True, text=True
+    ).stdout
 )
 
 for router in openwrt_config["router_list"]:
     # Copy the repo
     subprocess.run(
-        f"rsync -4 --mkpath --chown root:root -a --delete --exclude '.git' --exclude 'config' ../submodules/zapret/ root@{router}:/opt/zapret",
+        f"rsync -4 --mkpath --chown root:root -a --delete --exclude '.git' --exclude 'config' .decrypted.zapret/ root@{router}:/opt/zapret",
         shell=True,
     )
 
     # Copy tmp config
-    shutil.copy("../submodules/zapret/config.default", ".env")
+    shutil.copy(".decrypted.zapret/config.default", ".env")
 
     # Change the config
     for key, value in openwrt_config["config"].items():
         set_key(".env", key, value)
 
-        # Copy the config
-        subprocess.run(
-            f"rsync -4 --chown root:root -a --delete .env root@{router}:/opt/zapret/config",
-            shell=True,
-        )
+    # Copy the config
+    subprocess.run(
+        f"rsync -4 --chown root:root -a --delete .env root@{router}:/opt/zapret/config",
+        shell=True,
+    )
 
 # TODO: Run non-interactive
 # Run the script
