525778adf9999b2c0d90e608544d39d7b0b36429
zhex <dev@zhex.cc>
2025-03-15 23:07:26 +0700

    ci: migrate to podman ssh for faster ci

--- ci/run.sh
+++ ci/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 eval `ssh-agent`
 sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
+export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
+export DOCKER_HOST=$CONTAINER_HOST
 uv run ci/deployment.py
