f92e455c825191383d0db4bb8cbbacd6c199cc2f
zhex <dev@zhex.cc>
2025-03-15 23:37:08 +0700

    fix: add override path for docker podman

--- ci/run.sh
+++ ci/run.sh
@@ -3,5 +3,6 @@ eval `ssh-agent`
 sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
 export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
 export DOCKER_HOST=$CONTAINER_HOST
+export PATH=$HOME/.local/bin:$PATH
 uv run .devcontainer/main.py --profile="ci-host"
 uv run ci/deployment.py
