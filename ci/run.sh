406756ac8fc6088e78658d2f174a26b5a61b2c5c
zhex <dev@zhex.cc>
2025-05-06 17:45:40 +0700

    ci: fix missing path

--- ci/run.sh
+++ ci/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 eval `ssh-agent`
+export PATH="$(aqua root-dir)/bin:$HOME/.local/bin:$PATH"
 sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
 export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
-export PATH=$HOME/.local/bin:$PATH
 uv run ci/deployment.py
