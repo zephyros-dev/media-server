8cafaa765b943b258e1b6db0c834ad85c6e05f5a
zhex <me@zhex.dev>
2025-09-30 15:43:41 +0000

    ci: add ansible-playbook env

--- ci/run.sh
+++ ci/run.sh
@@ -4,4 +4,8 @@ eval $(mise activate --shims)
 sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
 # export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
 # uv run ci/deployment.py
+export ANSIBLE_CALLBACKS_ENABLED="timer"
+export ANSIBLE_DISPLAY_SKIPPED_HOSTS="False"
+export ANSIBLE_STDOUT_CALLBACK="dense"
+export ANSIBLE_HOST_KEY_CHECKING="False"
 uv run ansible-playbook main.yaml
