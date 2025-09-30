3b2bdd2f32b4b3521e8453af0243324726921213
zhex <me@zhex.dev>
2025-09-30 15:23:30 +0000

    ci: use host run instead of dagger

--- ci/run.sh
+++ ci/run.sh
@@ -2,5 +2,6 @@
 eval $(ssh-agent)
 eval $(mise activate --shims)
 sops -d secret/deployment/ssh_key.sops | ssh-add - &> /dev/null
-export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
-uv run ci/deployment.py
+# export CONTAINER_HOST=$(sops -d secret/deployment/podman.sops)
+# uv run ci/deployment.py
+uv run ansible-playbook main.yaml
